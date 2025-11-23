d98d33dc0d0c23fd586c24dea2c849b317c33863
khojwar <khojwartikaram@gmail.com>
2025-11-23 22:00:40 +0545

    add search and make header responsive

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,9 +1,12 @@
 'use client';
 import Link from 'next/link';
-import { Button } from '@mui/material';
+import { Button, Input } from '@mui/material';
 import { useAuth } from '../hooks/useAuth';
 import DarkModeToggle from "./DarkModeToggle";
 import { useTheme } from '@mui/material/styles';
+import { Menu } from 'lucide-react';
+import { Search } from 'lucide-react';
+import TextField from '@mui/material/TextField';
 
 export default function Header() {
   const { user, isAuthenticated, logout } = useAuth();
@@ -16,8 +19,23 @@ export default function Header() {
   return (
     <header className={headerClasses}>
       <div className="container mx-auto px-4 py-4 flex justify-between items-center">
-        <Link href="/" className={titleClasses}>MyBlog</Link>
-        <nav className="flex gap-4 items-center">
+        <div className='flex gap-2 justify-center items-center'>
+            <div className='md:hidden'><Menu /></div>
+            <Link href="/" className={titleClasses}>MyBlog</Link>
+        </div>
+        <div> 
+          <span className='md:hidden'><Search /></span> 
+          <span className='hidden md:block'> 
+            <TextField
+              id="standard-search"
+              label="Search"
+              type="search"
+              variant="standard"
+              className='md:w-56 lg:w-md'
+            />
+          </span> 
+        </div>
+        <nav className="hidden md:flex md:gap-4 md:items-center"> 
           {isAuthenticated ? (
             <>
               <Link href="/posts/new">
@@ -33,6 +51,7 @@ export default function Header() {
           )}
           <DarkModeToggle />
         </nav>
+        <div className='md:hidden'><DarkModeToggle /></div>
       </div>
     </header>
   );
