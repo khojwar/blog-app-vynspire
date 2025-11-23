58244ef1caee3f7a55eeeda55587ade0c016770e
khojwar <khojwartikaram@gmail.com>
2025-11-23 09:21:35 +0545

    useAuth hook is done

---
+++ src/hooks/useAuth.ts
@@ -0,0 +1,52 @@
+import { useSelector, useDispatch } from 'react-redux';
+import { RootState, AppDispatch } from '../store';
+import { loginSuccess, logout } from '../store/authSlice';
+import { useRouter } from 'next/navigation';
+import { useEffect } from 'react';
+import { jwtDecode } from "jwt-decode";
+
+export const useAuth = () => {
+    const { user, token, isAuthenticated } = useSelector((state: RootState) => state.auth);
+    const dispatch = useDispatch<AppDispatch>();
+    const router = useRouter();
+
+    useEffect(() => {
+    const token = localStorage.getItem('token');
+    if (token && !isAuthenticated) {  
+      try {
+        const decoded: any = jwtDecode(token);
+        dispatch(loginSuccess({ user: decoded, token }));
+      } catch (err) {
+        localStorage.removeItem('token');
+      }
+    }
+  }, [dispatch, isAuthenticated]);
+
+
+    const login = (user: any, token: string) => {
+        dispatch(loginSuccess({ user, token }));
+        router.push('/dashboard');
+    };
+
+    const logoutUser = () => {
+        dispatch(logout());
+        router.push('/login');
+    };
+
+    const requireAuth = () => {
+        if (!isAuthenticated) router.push('/login');
+    };
+
+    return {
+        user,
+        token,
+        isAuthenticated,
+        login,
+        logout: logoutUser,
+        requireAuth,
+    };
+
+
+
+
+}
